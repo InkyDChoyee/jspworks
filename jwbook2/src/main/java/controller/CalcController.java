7362d4283ac0697ae406b9b3af8bbdc713f07825
InkyDChoyee <choyee0421@naver.com>
2023-11-27 14:58:53 +0900

    feat.servlet calc result page

--- jwbook2/src/main/java/controller/CalcController.java
+++ jwbook2/src/main/java/controller/CalcController.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,10 +19,9 @@ public class CalcController extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 3. 컨텐츠 유형 보내기
+		
 		response.setContentType("text/html; charset=utf-8");
 		
-		PrintWriter out = response.getWriter();
 		
 		// 1. 폼에 입력된 데이터 받아오기
 		int n1 = Integer.parseInt(request.getParameter("n1"));
@@ -41,9 +41,20 @@ public class CalcController extends HttpServlet {
 			result = n1 / n2; break;
 		}
 		
-		// 3. 보내기
-		out.append("<html><body><h2>계산기</h2><hr>")
-		   .append("<p>계산결과: " + result + "</body></html>");
+		// 3. 컨텐츠 유형 보내기
+		/*
+		 * PrintWriter out = response.getWriter();
+		 * out.append("<html><body><h2>계산기</h2><hr>") .append("<p>계산결과: " + result +
+		 * "</body></html>");
+		 */
+		
+		// 계산 결과 - 모델(Model)
+		request.setAttribute("result", result);
+		// 결과를 받아줄 페이지
+		RequestDispatcher dispathcer = 
+				request.getRequestDispatcher("/mvc/calcResult.jsp");
+		
+		dispathcer.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
