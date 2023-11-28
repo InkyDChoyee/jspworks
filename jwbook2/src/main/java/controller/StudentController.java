1a663539b296abdcc1c70c3c3a4201488c8c1a35
InkyDChoyee <choyee0421@naver.com>
2023-11-28 16:01:50 +0900

    feat.student select

--- jwbook2/src/main/java/controller/StudentController.java
+++ jwbook2/src/main/java/controller/StudentController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,12 +10,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import student.Student;
+import student.StudentDAO;
+
 @WebServlet("*.do")   // '/'루트 경로 아래에 do로 끝나는 확장자인 파일은 모두 올 수 있음
 public class StudentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	// 객체 생성
+	StudentDAO sDAO;
        
     public StudentController() {
-        super();
+    	sDAO = new StudentDAO();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -35,6 +41,11 @@ public class StudentController extends HttpServlet {
 		String nextPage = "";
 		
 		if(command.equals("/studentlist.do")) {
+			List<Student> students = sDAO.getStudentList();
+			
+			// 모델 생성
+			request.setAttribute("students", students);
+			// 뷰 페이지로 이동
 			nextPage = "/student/studentlist.jsp";
 		}
 			
--- jwbook2/src/main/webapp/student/studentlist.jsp
+++ jwbook2/src/main/webapp/student/studentlist.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,7 +19,16 @@
 				<th>생일</th>
 				<th>이메일</th>
 			</tr>
-			
+			<c:forEach items="${students}" var="s" varStatus="i">
+			<tr>
+				<td>${i.count}</td>
+				<td>${s.username}</td>
+				<td>${s.univ}</td>
+				<td>${s.birth}</td>
+				<td>${s.email}</td>
+				
+			</tr>
+			</c:forEach>
 		</table>
 	</section>
 
