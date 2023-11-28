2784f4cd857dfe3b0fd55a0a539b46f11cd96bb9
InkyDChoyee <choyee0421@naver.com>
2023-11-28 16:55:20 +0900

    feat.student insert

--- jwbook2/src/main/java/controller/StudentController.java
+++ jwbook2/src/main/java/controller/StudentController.java
@@ -28,6 +28,11 @@ public class StudentController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 한글 인코딩
+		request.setCharacterEncoding("utf-8");
+		// 컨텐츠 유형 응답하기
+		response.setContentType("text/html; charset=utf-8");
+		
 		// 경로(uri) 설정 - 커맨드패턴(command)
 		// http:localhost:8000/student/studentlist.jsp
 		// 맨 뒤 경로 추출 - lastIndexOf('/'), subString(0)
@@ -48,7 +53,24 @@ public class StudentController extends HttpServlet {
 			// 뷰 페이지로 이동
 			nextPage = "/student/studentlist.jsp";
 		} else if(command.equals("/studentform.do")) {
+			// 폼에 입력된 데이터 받기
 			nextPage = "/student/studentform.jsp";
+		} else if(command.equals("/insertstudent.do")) {
+			// 폼에 입력된 데이터 받기
+			String username = request.getParameter("username");
+			String univ = request.getParameter("univ");
+			String birth = request.getParameter("birth");
+			String email = request.getParameter("email");
+			
+			// db에 저장
+			Student s = new Student();
+			s.setUsername(username);
+			s.setUniv(univ);
+			s.setBirth(birth);
+			s.setEmail(email);
+			
+			sDAO.insertStudent(s);
+			nextPage = "/studentlist.do";
 		}
 			
 		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
